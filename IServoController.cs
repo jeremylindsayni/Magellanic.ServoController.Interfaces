b0a689fcf1ae3553d614f649fbc83afb01f81b91
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-05-15 01:09:24 +0100

    Initial commit of Servo interface code.

---
+++ IServoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Magellanic.ServoController.Interfaces
+{
+    public interface IServoController : IDisposable
+    {
+        int Frequency { get; set; }
+
+        double MaximumDutyCycle { get; set; }
+
+        double MinimumDutyCycle { get; set; }
+
+        int ServoPin { get; set; }
+
+        Task Connect();
+
+        void Go();
+
+        IServoController SetPosition(int degree);
+
+        IServoController AllowTimeToMove(int pauseInMs);
+    }
+}
